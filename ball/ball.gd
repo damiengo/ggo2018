2c50d49c578ab6b7738f881290bec8a62b9d922b
10contre11 <d@gou.geon.fr>
2018-11-05 22:11:03 +0100

    walls rebound

--- ball/ball.gd
+++ ball/ball.gd
@@ -2,11 +2,17 @@ extends KinematicBody2D
 
 # Example: https://www.youtube.com/watch?v=4CLvL05Av6g
 
-var speed = 50
-var movedir = Vector2(0,0)
+var speed = 400
+var velocity = Vector2(0,0)
+
+func _ready():
+	velocity.x = 1
+	velocity.y = 1
 
 func _physics_process(delta):
-	movedir.x = 1
-	movedir.y = 1
-	var motion = movedir.normalized() * speed
-	move_and_slide(motion, Vector2(0,0))	
+	print(velocity)
+	var motion = delta * velocity * speed
+	
+	var collision = move_and_collide(motion)
+	if collision:
+		velocity = velocity.bounce(collision.normal)
