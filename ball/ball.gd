e470509684e0348f165cdafc9efac720e5686d72
10contre11 <d@gou.geon.fr>
2018-11-08 22:34:11 +0100

    block moving

--- ball/ball.gd
+++ ball/ball.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 # Example: https://www.youtube.com/watch?v=4CLvL05Av6g
 
-var speed = 400
+var speed = 800
 var velocity = Vector2(0,0)
 
 func _ready():
@@ -10,8 +10,7 @@ func _ready():
 	velocity.y = 1
 
 func _physics_process(delta):
-	var motion = delta * velocity * speed
-	
+	var motion = delta * velocity * speed	
 	var collision = move_and_collide(motion)
 	if collision:
 		velocity = velocity.bounce(collision.normal)
--- game/main.gd
+++ game/main.gd
@@ -1,3 +1,4 @@
+tool
 extends Node
 
 var _block_size = 32
@@ -6,16 +7,22 @@ var _block_types = [
 	preload("res://blocks/t.tscn")
 ]
 
+var _block_velocity = Vector2(0,0)
+
 var _falling_blocks = []
 
 func _ready():
+	_block_velocity.x = 1
+	_block_velocity.y = 1
 	var block_resource = _block_types[0]
 	var block = block_resource.instance()
 	block.position = Vector2(_block_size * 5, _block_size * 3)
 	add_child(block)
 	_falling_blocks.append(block)
+	print(block.position)
 
-func _process(delta):
+func _physics_process(delta):
 	var speed = 20
 	for falling_block in _falling_blocks:
-		falling_block.move_local_y(_block_size*delta*speed)
+		#falling_block.move_local_y(_block_size*delta*speed)
+		falling_block.move_and_collide(_block_velocity*delta*speed) # a sortir dans un script du block?
