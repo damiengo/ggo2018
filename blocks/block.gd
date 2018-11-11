7b696cd3339f02fed3e5dfc57c3e830b8c724926
10contre11 <d@gou.geon.fr>
2018-11-11 22:40:34 +0100

    direction

--- blocks/block.gd
+++ blocks/block.gd
@@ -8,9 +8,18 @@ var velocity = Vector2(0,0)
 func _ready():
 	velocity.x = 0
 	velocity.y = 1
+	set_process_input(true) 
 
 func _physics_process(delta):
-	if Input.is_action_pressed("move_left"): print("left")
-	if Input.is_action_pressed("move_right"): position.x = position.x+32
 	var motion = delta * velocity * speed	
 	var collision = move_and_collide(motion)
+
+func _input(event):
+	var move_left = event.is_action_pressed("ui_left")
+	var move_right = event.is_action_pressed("ui_right")
+	
+	if move_left:
+		print("Move left")
+	
+	if move_right:
+		print("Move right")
