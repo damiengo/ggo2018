be635c76d65f265ec178e7d1417dd9237d7e6b81
10contre11 <d@gou.geon.fr>
2018-11-15 22:00:32 +0100

    added block

--- game/main.gd
+++ game/main.gd
@@ -3,7 +3,13 @@ extends Node
 var _block_size = 32
 
 var _block_types = [
-	preload("res://blocks/s.tscn")
+	preload("res://blocks/j.tscn"),
+	preload("res://blocks/l.tscn"),
+	preload("res://blocks/i.tscn"),
+	preload("res://blocks/o.tscn"),
+	preload("res://blocks/s.tscn"),
+	preload("res://blocks/t.tscn"),
+	preload("res://blocks/z.tscn")
 ]
 
 var _falling_blocks = []
@@ -14,4 +20,3 @@ func _ready():
 	block.position = Vector2(_block_size * 5, _block_size * 8)
 	add_child(block)
 	_falling_blocks.append(block)
-	print(block.position)
